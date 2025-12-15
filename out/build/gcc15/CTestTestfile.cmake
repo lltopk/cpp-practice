02af41f207fedf9bf6fa2ad374c9215e48e39b14
lltopk <liuyanoutsee@outlook.com>
2025-12-15 16:18:10 +0800

    Stop tracking out/

--- out/build/gcc15/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/github/cpp-practice
-# Build directory: E:/github/cpp-practice/out/build/gcc15
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
